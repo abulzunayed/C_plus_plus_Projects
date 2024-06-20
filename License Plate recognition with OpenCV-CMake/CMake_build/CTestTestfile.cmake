082bd8968b759e61875d6911be317a60f6378049
Abul Kasem Mohammad Zunayed <abulzunayed@gmail.com>
2024-06-20 21:10:33 +0200

    Add files via upload

---
+++ License Plate recognition with OpenCV-CMake/CMake_build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/C++project/computer_project
+# Build directory: E:/C++project/computer_project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
