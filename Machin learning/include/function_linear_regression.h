ef3f7893eef2f335e115796293b06aa8d750983b
Abul Kasem Mohammad Zunayed <abulzunayed@gmail.com>
2024-07-06 23:55:45 +0200

    Add files via upload

---
+++ Machin learning/include/function_linear_regression.h
@@ -0,0 +1,12 @@
+#ifndef LINEAR_REGRESSION_H_INCLUDE
+#define LINEAR_REGRESSION_H_INCLUDE
+
+
+/* this custom sort function is defined to  sort on basis of min absolute value or error*/
+
+
+bool custom_sort(double a, double b);
+void Test_data(double b0, double b1);
+
+
+#endif
