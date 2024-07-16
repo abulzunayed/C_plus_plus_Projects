d055d3fd19da979cd5c74b7ffd264d570ad53dbb
wo65beja <abul.km.zunayed@fau.de>
2024-07-16 09:39:59 +0200

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/C++project/Image_processing_opencv
+# Build directory: E:/C++project/Image_processing_opencv/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ webcam.cpp
@@ -0,0 +1,55 @@
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include<iostream>
+
+using namespace cv;
+using namespace std;
+
+
+///////////////// Import test Images  //////////////////////
+
+/* int main() {
+
+	string path = "E:/C++project/Image_processing_opencv/image/test.jpg";
+	Mat img = imread(path);      // here, mat is matrix data type.
+	imshow("Image", img);
+	waitKey(0);
+
+} */
+
+///////////////  Import Video  //////////////////////
+
+/* void main() 
+{
+
+	string path = "E:/C++project/Image_processing_opencv/image/test_video.mp4";
+	VideoCapture cap(path);
+	Mat img;
+
+	while (true) {
+
+		cap.read(img);
+		imshow("image", img);
+		waitKey(10);
+	}
+} */
+
+/////////////////  Webcam  //////////////////////
+
+void main() 
+{
+
+	VideoCapture cap(0);
+	Mat img;
+
+	while (true) {
+
+		cap.read(img);
+		imshow("Image", img);
+		waitKey(1);
+	}
+}
+
+
+
